07a55d6df86f12ff0d1bb6c376195ec83d05d22a
GabrielLock <gabrielferreiralockvieira@gmail.com>
2023-04-26 08:38:04 -0300

    repetitions

---
+++ exemplos/ex009/ambiente.js
@@ -0,0 +1,25 @@
+/*  while
+
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+*/
+
+/*  do 
+
+var d = 1
+do {
+    console.log(`Passo ${d}`)
+    d++
+}while (d <= 6)
+
+*/
+
+console.log('Vai começar...')
+for(var c = 1; c <= 10; c++){
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')
